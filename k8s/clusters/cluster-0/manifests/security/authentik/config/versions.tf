8e6df38532e77041ca2e9c0e39dfe5e437936db4
tvories <taylor@tmtech.me>
2026-01-22 12:24:21 -0700

    fix: stupid tokens
    Signed-off-by: tvories <taylor@tmtech.me>

--- k8s/clusters/cluster-0/manifests/security/authentik/config/versions.tf
+++ k8s/clusters/cluster-0/manifests/security/authentik/config/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     authentik = {
       source  = "goauthentik/authentik"
-      version = "2025.12.0"
+      version = "2025.6.0"
     }
     onepassword = {
       source  = "1Password/onepassword"
-      version = "3.1.2"
+      version = "2.1.2"
     }
   }
 }
@@ -14,6 +14,6 @@ terraform {
 provider "authentik" {
   # url   = var.authentik_url
   url = "https://authentik.t-vo.us"
-  # token = var.authentik_token
-  token = "v5UV7xiykmYKDFQjajR5pbxnYhptVumTeCYlgYz5IioOa4BNd3x6I0iE5viU"
+  token = var.authentik_token
+  # token = "irevokedmylasttokensorry"
 }
